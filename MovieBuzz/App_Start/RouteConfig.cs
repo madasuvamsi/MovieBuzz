5329c2464d07261515b1b284bd7d7aee68ed06b1
madasuvamsi <madasuvamsi@gmail.com>
2020-05-18 19:10:30 -0700

    Added Attribute Routing

--- MovieBuzz/App_Start/RouteConfig.cs
+++ MovieBuzz/App_Start/RouteConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -12,6 +13,15 @@ namespace MovieBuzz
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapMvcAttributeRoutes();
+
+            //routes.MapRoute(
+            //    name:"ReleaseByDate",
+            //    url:"Movie/Release/{year}/{month}",
+            //    defaults: new {Controller="Movie",action= "ReleaseByDate" },
+            //    new {year=@"2015|2016",month=@"\d{2}"}
+            //    );
 
             routes.MapRoute(
                 name: "Default",
--- MovieBuzz/Controllers/MovieController.cs
+++ MovieBuzz/Controllers/MovieController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure.MappingViews;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,5 +16,15 @@ namespace MovieBuzz.Controllers
             var movie = new Movies { movieName = "Avengers" };
             return View(movie);
         }
+
+        //Movie/Release
+        [Route("Movie/Released/{year:regex(\\d{4})}/{month:regex(\\d{2})}")]
+
+        public ActionResult ReleaseByDate(int year,int month)
+        {
+            return Content(string.Format("{0}/{1}", year, month));
+        }
+
+        
     }
 }
