651274f884c3cf60593e02dd333e00cbc609c94f
madasuvamsi <madasuvamsi@gmail.com>
2020-05-18 18:40:07 -0700

    Added Bootstrap theme and Movie controller

---
+++ MovieBuzz/Controllers/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieBuzz.Models;
+
+namespace MovieBuzz.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie/Random
+        public ActionResult Random()
+        {
+            var movie = new Movies { movieName = "Avengers" };
+            return View(movie);
+        }
+    }
+}
---
+++ MovieBuzz/Models/Movies.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieBuzz.Models
+{
+    public class Movies
+    {
+        public int id { get; set; }
+        public string  movieName { get; set; }
+    }
+}
